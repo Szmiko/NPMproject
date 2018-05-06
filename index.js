bfbead1efcb1dff09e7f8c975d63d5f1b7bbbf44
Szymon Grabowski <szmiko@wp.pl>
2018-05-06 16:15:09 +0200

    Task

---
+++ index.js
@@ -0,0 +1,29 @@
+var os = require('os');
+var OSinfo = require('./modules/OSinfo');
+
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', function() {
+	var input = process.stdin.read();
+	if (!input) {
+		return;
+	};
+
+	var instruction = input.trim();
+	switch(instruction) {
+		case '/exit':
+			process.stdout.write('Quitting app!\n');
+			process.exit();
+		break;
+		case '/sayhello':
+			process.stdout.write('hello!\n');
+		break;
+		case '/getOSinfo':
+			OSinfo.print(); 
+		break;
+		case '/toTime':
+			toTime.print();
+		break;
+		default:
+			process.stderr.write('Wrong instruction!\n');
+		};
+});
